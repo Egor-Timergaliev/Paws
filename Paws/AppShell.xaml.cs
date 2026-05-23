e303e26500679e1a05d3c0ece70b7c1f486031e9
admin <admin@MYPC11>
2026-05-23 09:36:49 +0500

    Добавьте файлы проекта.

---
+++ Paws/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Paws
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Paws/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace Paws
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            count = Preferences.Get("counter_value", 0);
+
+            CounterLabel.Text = count.ToString();
+        }
+
+        private void OnPlusClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            CounterLabel.Text = count.ToString();
+
+            Preferences.Set("counter_value", count);
+        }
+
+        private void OnResetClicked(object sender, EventArgs e)
+        {
+            count = 0;
+
+            CounterLabel.Text = count.ToString();
+
+            Preferences.Set("counter_value", count);
+        }
+    }
+}
